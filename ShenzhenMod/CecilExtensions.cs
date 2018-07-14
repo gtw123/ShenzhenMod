5442eb9ce5e0b9afa9491dac9b95283e8fc94ab8
gtw123 <39769164+gtw123@users.noreply.github.com>
2018-07-15 02:13:02 +0800

    More work in progress

--- ShenzhenMod/CecilExtensions.cs
+++ ShenzhenMod/CecilExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using Mono.Cecil;
 using Mono.Cecil.Cil;
@@ -7,31 +8,67 @@ namespace ShenzhenMod
 {
     public static class CecilExtensions
     {
-        public static TypeDefinition FindType(this ModuleDefinition module, string name)
+        public static FieldDefinition FindField(this ModuleDefinition module, string typeName, string fieldName)
         {
-            return module.GetType(name) ?? throw new Exception($"Cannot find type \"{name}\"");
+            var type = module.FindType(typeName);
+            return type.Fields.SingleOrDefault(m => m.Name == fieldName) ?? throw new Exception($"Cannot find field \"{fieldName}\" in type \"{type.Name}\"");
         }
 
         public static MethodDefinition FindMethod(this ModuleDefinition module, string typeName, string methodName)
         {
-            var type = FindType(module, typeName);
+            return module.FindType(typeName).FindMethod(methodName);
+        }
+
+        public static MethodDefinition FindMethod(this TypeDefinition type, string methodName)
+        {
             var method = type.Methods.Where(m => m.Name == methodName);
             if (method.Count() == 0)
             {
-                throw new Exception($"Cannot find method \"{methodName}\" in type \"{typeName}\"");
+                throw new Exception($"Cannot find method \"{methodName}\" in type \"{type.Name}\"");
             }
             else if (method.Count() > 1)
             {
-                throw new Exception($"Found more than one method called \"{methodName}\" in type \"{typeName}\"");
+                throw new Exception($"Found more than one method called \"{methodName}\" in type \"{type.Name}\"");
             }
 
             return method.First();
         }
 
+        public static TypeDefinition FindType(this ModuleDefinition module, string name)
+        {
+            return module.GetType(name) ?? throw new Exception($"Cannot find type \"{name}\"");
+        }
+
+        public static Instruction FindInstructionAtOffset(this MethodDefinition method, int offset, OpCode opCode, object operand)
+        {
+            var instr = method.Body.Instructions.SingleOrDefault(i => i.Offset == offset) ?? throw new Exception($"Cannot find instruction at offset IL_{offset:X4} in method \"{method.Name}\"");
+            if (!instr.Matches(opCode, operand))
+            {
+                throw new Exception($"Instruction at offset IL_{offset:X4} in method \"{method.Name}\" does not match OpCode \"{opCode}\" and operand \"{operand}\"");
+            }
+
+            return instr;
+        }
+
+        public static IEnumerable<Instruction> FindInstructions(this MethodDefinition method, OpCode opCode, object operand, int numExpected)
+        {
+            var instr = FindInstructions(method, opCode, operand);
+            if (instr.Count() != numExpected)
+            {
+                throw new Exception($"Expected to find {numExpected} instructions with OpCode \"{opCode}\" and operand \"{operand}\" in method \"{method.Name}\", but found {instr.Count()}");
+            }
+
+            return instr;
+        }
+
+        public static IEnumerable<Instruction> FindInstructions(this MethodDefinition method, OpCode opCode, object operand)
+        {
+            return method.Body.Instructions.Where(i => i.Matches(opCode, operand));
+        }
+
         public static Instruction FindInstruction(this MethodDefinition method, OpCode opCode, object operand)
         {
-            var instr = method.Body.Instructions.Where(i => i.OpCode == opCode && i.Operand.Equals(operand));
-
+            var instr = FindInstructions(method, opCode, operand);
             if (instr.Count() == 0)
             {
                 throw new Exception($"Cannot find instruction with OpCode \"{opCode}\" and operand \"{operand}\" in method \"{method.Name}\"");
@@ -42,6 +79,40 @@ namespace ShenzhenMod
             }
 
             return instr.First();
+        }
+
+        public static bool Matches(this Instruction instruction, OpCode opCode, object operand)
+        {
+            return instruction.OpCode == opCode && Object.Equals(instruction.Operand, operand);
+        }
+
+        public static void Set(this Instruction instruction, OpCode opCode, object operand)
+        {
+            instruction.OpCode = opCode;
+            instruction.Operand = operand;
+        }
+
+        public static IEnumerable<Instruction> RemoveRange(this ILProcessor il, Instruction start, Instruction end)
+        {
+            var removed = new List<Instruction>();
+            var current = start;
+            while (current != end)
+            {
+                var next = current.Next;
+                il.Remove(current);
+                removed.Add(current);
+                current = next;
+            }
+
+            return removed;
+        }
+
+        public static void InsertBefore(this ILProcessor il, Instruction target, IEnumerable<Instruction> instructions)
+        {
+            foreach (var instr in instructions)
+            {
+                il.InsertBefore(target, instr);
+            }
         }
     }
 }
--- ShenzhenMod/ShenzhenPatcher.cs
+++ ShenzhenMod/ShenzhenPatcher.cs
@@ -5,6 +5,7 @@ using System.IO;
 using System.Linq;
 using Mono.Cecil;
 using Mono.Cecil.Cil;
+using Mono.Cecil.Rocks;
 
 namespace ShenzhenMod
 {
@@ -13,6 +14,8 @@ namespace ShenzhenMod
         private static readonly log4net.ILog sm_log = log4net.LogManager.GetLogger(typeof(ShenzhenPatcher));
 
         private ModuleDefinition m_module;
+        private MethodDefinition m_puzzleGetSize;
+        private MethodDefinition m_puzzleSetSize;
 
         public ShenzhenPatcher(string unpatchedPath)
         {
@@ -34,6 +37,8 @@ namespace ShenzhenMod
             ChangeMaxPuzzleSize();
             ChangeScrollSize();
             AddSizeToPuzzle();
+            FixTileGeneration();
+            FixTraceLoading();
         }
 
         private void ChangeMaxPuzzleSize()
@@ -52,18 +57,25 @@ namespace ShenzhenMod
 
         private void AddSizeToPuzzle()
         {
-            var type = m_module.FindType("Puzzle");
+            var puzzleType = m_module.FindType("Puzzle");
             var index2DType = m_module.FindType("Index2");
             var sizeField = new FieldDefinition("size", FieldAttributes.Private, index2DType);
-            type.Fields.Add(sizeField);
+            puzzleType.Fields.Add(sizeField);
             var isSizeSetField = new FieldDefinition("isSizeSet", FieldAttributes.Private, m_module.TypeSystem.Boolean);
-            type.Fields.Add(isSizeSetField);
+            puzzleType.Fields.Add(isSizeSetField);
 
-            AddSetSize();
-            AddGetSize();
+            m_puzzleSetSize = AddSetSize();
+            m_puzzleGetSize = AddGetSize();
 
-            void AddSetSize()
+            MethodDefinition AddSetSize()
             {
+                /*
+                    public void SetSize(Index2 size)
+                    {
+                        this.size = size;
+                        isSizeSet = true;
+                    }
+                */
                 var method = new MethodDefinition("SetSize", MethodAttributes.Public, m_module.TypeSystem.Void);
                 method.Parameters.Add(new ParameterDefinition("size", ParameterAttributes.None, index2DType));
                 var il = method.Body.GetILProcessor();
@@ -74,10 +86,12 @@ namespace ShenzhenMod
                 il.Emit(OpCodes.Ldc_I4_1);
                 il.Emit(OpCodes.Stfld, isSizeSetField);
                 il.Emit(OpCodes.Ret);
-                type.Methods.Add(method);
+                puzzleType.Methods.Add(method);
+
+                return method;
             }
-            
-            void AddGetSize()
+
+            MethodDefinition AddGetSize()
             {
                 /*
                     public Index2 GetSize()
@@ -101,16 +115,181 @@ namespace ShenzhenMod
                 il.Append(label1);
                 il.Emit(OpCodes.Ldc_I4_S, (sbyte)22);
                 il.Emit(OpCodes.Ldc_I4_S, (sbyte)14);
-                il.Emit(OpCodes.Newobj, index2DType.Methods.Single(m => m.Name == ".ctor"));
+                il.Emit(OpCodes.Newobj, index2DType.FindMethod(".ctor"));
                 il.Emit(OpCodes.Ret);
-                type.Methods.Add(method);
+                puzzleType.Methods.Add(method);
+
+                return method;
             }
         }
-        /*
-        private void sds()
+        
+        private void FixTileGeneration()
         {
-.#=qWo_Ilq5Sos4EQLLY_xDRAjAw6Q83Z6ZpjGeSwvBaB5U=.#=qAmFcmFbUxPAB0DUb13KNOEmJgRvZSM6E$AgqIvIwrnk=
-        }*/
+            var method = m_module.FindMethod("#=qWo_Ilq5Sos4EQLLY_xDRAjAw6Q83Z6ZpjGeSwvBaB5U=", "#=qAmFcmFbUxPAB0DUb13KNOEmJgRvZSM6E$AgqIvIwrnk=");
+            var il = method.Body.GetILProcessor();
+
+            var instructionsToReplace = method.FindInstructions(OpCodes.Ldsfld, m_module.FindField("#=qL_uhZp1CYmicXxDRy$c1Bw==", "#=qAVzUnhNiHUMBOnnky4iCCA=="), 2);
+            foreach (var instr in instructionsToReplace.ToList())
+            {
+                il.InsertBefore(instr, il.Create(OpCodes.Ldarg_0));
+                il.InsertBefore(instr, il.Create(OpCodes.Call, m_puzzleGetSize));
+                il.Remove(instr);
+            }
+        }
+
+        private void FixTraceLoading()
+        {
+            var method = m_module.FindMethod("Solution", "#=qMQ0eadM7OBun57pFLO1wWA==");
+            var il = method.Body.GetILProcessor();
+
+            FixTraceLoading1();
+            FixTraceLoading2();
+
+            // Fix up branches instructions that can no longer be "short" branches
+            method.Body.SimplifyMacros();
+            method.Body.OptimizeMacros();
+
+            void FixTraceLoading1()
+            {
+                /*
+			        if (index == new Index2(0, 2))
+			        {
+				        for (int j = 0; j < 22; j++)
+				        {
+					        for (int k = 0; k < 11; k++)
+					        {
+						        this.Traces.AddTrace(new Index2(j, k), this.Traces.GetTrace(new Index2(j, k + 3)));
+					        }
+					        for (int l = 11; l < 14; l++)
+					        {
+						        this.Traces.AddTrace(new Index2(j, l), this.Trace.None);
+					        }
+				        }
+        			}
+
+                    int lastY = index.Y;
+                    if (lastY > 0)
+                    {
+                        int maxY = this.Traces.GetSize().Y;
+                        for (int j = 0; j < this.Traces.GetSize().X; j++)
+                        {
+                            for (int k = 0; k < maxY - lastY; k++)
+                            {
+                                this.Traces.AddTrace(new Index2(j, k), this.Traces.GetTrace(new Index2(j, k + lastY)));
+                            }
+                            for (int l = maxY - lastY; l < maxY; l++)
+                            {
+                                this.Traces.AddTrace(new Index2(j, l), Trace.None);
+                            }
+                        }
+                    }
+
+                 */
+
+                // Remove the "if (index == new Index2(0, 2))" check, but leave the first instruction so we don't break branches
+                var ifIndexEquals02 = method.FindInstructionAtOffset(0x04be, OpCodes.Ldloc_S, method.Body.Variables[13]);
+                var jLoopStart = method.FindInstructionAtOffset(0x04ce, OpCodes.Ldc_I4_0, null);
+                il.RemoveRange(ifIndexEquals02.Next, jLoopStart);
+
+                // Add two new variables
+                var lastY = new VariableDefinition(m_module.TypeSystem.Int32);
+                method.Body.Variables.Add(lastY);
+                var maxY = new VariableDefinition(m_module.TypeSystem.Int32);
+                method.Body.Variables.Add(maxY);
+
+                // Initialize the two new variables and add the "if (lastY > 0)" check
+                var index2Type = m_module.FindType("Index2");
+                ifIndexEquals02.Set(OpCodes.Ldloca_S, method.Body.Variables[13]);
+                il.InsertBefore(jLoopStart, il.Create(OpCodes.Ldfld, index2Type.Fields[2]));
+                il.InsertBefore(jLoopStart, il.Create(OpCodes.Stloc_S, lastY));
+                il.InsertBefore(jLoopStart, il.Create(OpCodes.Ldloc_S, lastY));
+                il.InsertBefore(jLoopStart, il.Create(OpCodes.Ldc_I4_0));
+                il.InsertBefore(jLoopStart, il.Create(OpCodes.Ble, method.Body.Instructions.Last()));
+                il.InsertBefore(jLoopStart, il.Create(OpCodes.Ldarg_0));
+                il.InsertBefore(jLoopStart, il.Create(OpCodes.Ldfld, m_module.FindField("Solution", "#=qoToRfupqxh4PHUk11ckgIg==")));
+                il.InsertBefore(jLoopStart, il.Create(OpCodes.Callvirt, m_module.FindMethod("#=qU2wvld4wYwd2RmifHjQEOQ==", "#=qa$TS_$HdBzzP07FjV63Yrw==")));
+                il.InsertBefore(jLoopStart, il.Create(OpCodes.Ldfld, index2Type.Fields[2]));
+                il.InsertBefore(jLoopStart, il.Create(OpCodes.Stloc_S, maxY));
+
+                // Replace "k + 3" with "k + lastY"
+                method.FindInstruction(OpCodes.Ldc_I4_3, null).Set(OpCodes.Ldloc_S, lastY);
+
+                // Replace "l < 14" with "l < maxY"
+                method.FindInstruction(OpCodes.Ldc_I4_S, (sbyte)14).Set(OpCodes.Ldloc_S, maxY);
+
+                // Replace "11" with "maxY - lastY"
+                foreach (var instr in method.FindInstructions(OpCodes.Ldc_I4_S, (sbyte)11, 2).ToList())
+                {
+                    instr.Set(OpCodes.Ldloc_S, maxY);
+                    il.InsertAfter(instr, il.Create(OpCodes.Sub));
+                    il.InsertAfter(instr, il.Create(OpCodes.Ldloc_S, lastY));
+                }
+
+                // Replace "22" with "this.Traces.GetSize().X"
+                var instr2 = method.FindInstruction(OpCodes.Ldc_I4_S, (sbyte)22);
+                instr2.Set(OpCodes.Ldarg_0, null);
+                il.InsertAfter(instr2, il.Create(OpCodes.Ldfld, index2Type.Fields[1]));
+                il.InsertAfter(instr2, il.Create(OpCodes.Callvirt, m_module.FindMethod("#=qU2wvld4wYwd2RmifHjQEOQ==", "#=qa$TS_$HdBzzP07FjV63Yrw==")));
+                il.InsertAfter(instr2, il.Create(OpCodes.Ldfld, m_module.FindField("Solution", "#=qoToRfupqxh4PHUk11ckgIg==")));
+            }
+
+            void FixTraceLoading2()
+            {
+                /* Replace this:
+
+                    if (flag)
+                    {
+                        index2.x++;
+                        if (index2.x >= this.gclass108_0.method_0().x)
+                        {
+                            index2.x = 0;
+                            index2.y--;
+                            if (index2.y < 0)
+                            {
+                                break;
+                            }
+                        }
+                    }
+
+                with this:
+
+                    else if (chr == '\n')
+                    {
+                        index2.x = 0;
+                        index2.y--;
+                        if (index2.y < 0)
+                        {
+                            break;
+                        }
+                    }
+                    if (flag)
+                    {
+                        index2.x++;
+                    }
+                */
+
+                var ifFlag = method.FindInstructionAtOffset(0x0463, OpCodes.Ldloc_S, method.Body.Variables[16]);
+                var ifXGreater = method.FindInstructionAtOffset(0x0473, OpCodes.Ldloc_S, method.Body.Variables[13]);
+                var handleEOL = method.FindInstructionAtOffset(0x048c, OpCodes.Ldloca_S, method.Body.Variables[13]);
+                var loopBodyEnd = method.FindInstructionAtOffset(0x04aa, OpCodes.Ldloc_S, method.Body.Variables[4]);
+
+                // If the if before "if (flag)" is true, jump to the "if (flag" rather than falling through
+                il.InsertBefore(ifFlag, il.Create(OpCodes.Br, ifFlag));
+
+                // Remove some of the instructions from the "if (flag)" block and shift some of them towards the bottom of the loop
+                var removed = il.RemoveRange(ifFlag, handleEOL);
+                il.InsertBefore(loopBodyEnd, removed.TakeWhile(i => i != ifXGreater));
+
+                // Insert the '\n' check
+                var checkNewLine = il.Create(OpCodes.Ldloc_S, method.Body.Variables[15]);
+                il.InsertBefore(handleEOL, checkNewLine);
+                il.InsertBefore(handleEOL, il.Create(OpCodes.Ldc_I4_S, (sbyte)10));
+                il.InsertBefore(handleEOL, il.Create(OpCodes.Bne_Un, ifFlag));
+
+                // If the if before "if (flag)" is false, jump to our new '\n' check
+                method.FindInstructionAtOffset(0x044b, OpCodes.Brfalse_S, ifFlag).Operand = checkNewLine;
+            }
+        }
 
         public void SavePatchedFile(string targetFile)
         {
